f01dd03d44c122b0fb35211252edd2cc8f60d986
Zwakkei <wackyramayla@gmail.com>
2026-05-12 05:00:00 +0800

    Updated Database Configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,11 +62,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
-}
+DATABASES = {}
+if os.getenv('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+else:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 
 
 # Password validation
