d57707badd002e61c925d8d24f4a1a0baee21f75
Zwakkei <wackyramayla@gmail.com>
2026-05-12 05:05:19 +0800

    Updated Database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,14 +62,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
-DATABASES = {}
-if os.getenv('DATABASE_URL'):
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-else:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True # Supabase requires SSL
+    )
+}
 
 
 # Password validation
